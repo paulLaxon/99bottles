0967a8de6b2f270ae9220e7519d3860a4239942c
Paul Laxon <pdl1963@gmail.com>
2022-03-16 11:46:27 -0400

    initial tests and simple green code

---
+++ bottles.rb
@@ -0,0 +1,30 @@
+# bottle verse class
+class BottleVerse
+  def lyrics(bottles_at_start, bottles_at_end)
+    verses = ''
+    bottles_at_start.downto(bottles_at_end) do |bottles|
+      verses += verse(bottles)
+    end
+    verses
+  end
+
+  def verse(bottles)
+    case bottles
+    when 0
+      'No more bottles of beer on the wall, no more bottles of beer.\n' \
+      'Go to the store and buy some more, 99 bottles of beer on the wall.\n'
+    when 1
+      "#{bottles} bottle of beer on the wall, " \
+      "#{bottles} bottle of beer.\\n" \
+      "Take one down and pass it around, no more bottles of beer on the wall.\\n"
+    when 2
+      "#{bottles} bottles of beer on the wall, " \
+      "#{bottles} bottles of beer.\\n" \
+      "Take one down and pass it around, #{bottles - 1} bottle of beer on the wall.\\n"
+    else
+      "#{bottles} bottles of beer on the wall, " \
+      "#{bottles} bottles of beer.\\n" \
+      "Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.\\n"
+    end
+  end
+end
