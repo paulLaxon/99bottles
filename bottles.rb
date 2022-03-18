bfb30d96e9f12b54b6bba1eadc272172978fa455
Paul Laxon <pdl1963@gmail.com>
2022-03-17 20:39:23 -0400

    ch 3 horizontal refactoring

--- bottles.rb
+++ bottles.rb
@@ -14,17 +14,21 @@ class BottleVerse
       'No more bottles of beer on the wall, no more bottles of beer.\n' \
       'Go to the store and buy some more, 99 bottles of beer on the wall.\n'
     when 1
-      "#{bottles} bottle of beer on the wall, " \
-      "#{bottles} bottle of beer.\\n" \
+      "#{bottles} #{container(bottles)} of beer on the wall, " \
+      "#{bottles} #{container(bottles)} of beer.\\n" \
       'Take one down and pass it around, no more bottles of beer on the wall.\\n'
-    when 2
-      "#{bottles} bottles of beer on the wall, " \
-      "#{bottles} bottles of beer.\\n" \
-      "Take one down and pass it around, #{bottles - 1} bottle of beer on the wall.\\n"
     else
-      "#{bottles} bottles of beer on the wall, " \
-      "#{bottles} bottles of beer.\\n" \
-      "Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.\\n"
+      "#{bottles} #{container(bottles)} of beer on the wall, " \
+      "#{bottles} #{container(bottles)} of beer.\\n" \
+      "Take one down and pass it around, #{bottles - 1} #{container(bottles - 1)} of beer on the wall.\\n"
+    end
+  end
+
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
     end
   end
 end
